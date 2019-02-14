2d16d48eaca847ad203839b3bda2e9d23ff14563
Harugot <33010386+Harugot@users.noreply.github.com>
2019-02-14 10:12:42 -0600

    rooms now create doors leading to adjacent rooms, starting room will remain open

--- Unity Engine Projects/Trio/Scripts/LevelSpawning/LevelSpawning.cs
+++ Unity Engine Projects/Trio/Scripts/LevelSpawning/LevelSpawning.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+//TODO -- REFACTOR ALL OF THIS
 public class LevelSpawning : MonoBehaviour {
 
     LevelFactory TestLevel;
@@ -19,18 +20,29 @@ public class LevelSpawning : MonoBehaviour {
     Vector3
         North = new Vector3(0, 0, 1),
         East = new Vector3(1, 0, 0),
-        South = new Vector3(0, 0, -1),
         West = new Vector3(-1, 0, 0),
         PreviousSpawnLocation,
         PreviousMoveDirection,
         CurrentMoveDirection;
 
+    public delegate void FinishedSpawning();
+    public static FinishedSpawning FinishedSpawningRooms;
+
     void Start () {
 
-        TestLevel = new LevelFactory(10, 15, true, true, true);
+        //Constructor for a new level, specifies the grid size, the max amount of rooms, whether it adheres strictly to the bounds of the grid
+        //if it should spawn more linear rooms, and whether or not the spawns should double back on each other - in that order
+        TestLevel = new LevelFactory(4, 15, true, false, false);
+
         roomFactory = GetComponent<RoomFactory>();
 
-        SetRoomLocation(TestLevel);
+        /*Spawning should happen in the following order
+         * Find an open location to spawn a room in
+         * Figure out which room type to spawn
+         * Spawn the room
+         */
+
+        FindNextRoomLocation(TestLevel);
         StartCoroutine(StartSpawning());
     }
 
@@ -40,11 +52,12 @@ public class LevelSpawning : MonoBehaviour {
         yield return new WaitForSeconds(0.10f);
         if (!TestLevel.EndRoomSpawned)
         {
-            SetRoomLocation(TestLevel);
+            FindNextRoomLocation(TestLevel);
             SpawnRoom(currentLocation, TestLevel);
             StartCoroutine(StartSpawning());
         }else
         {
+            FinishedSpawningRooms();
             StopAllCoroutines();
         }
     }
@@ -52,7 +65,7 @@ public class LevelSpawning : MonoBehaviour {
     //If we are just starting off, we want to pick a random location in the first row and spawn a starting room.
     //This is important because every room thereafter HAS to be linked to this room in some way.
     //If the starting room has already been spawned, start incrementing our location by 1 and moving.
-    void SetRoomLocation(LevelFactory level)
+    void FindNextRoomLocation(LevelFactory level)
     {
         if (!level.StartRoomSpawned)
         {
@@ -61,10 +74,36 @@ public class LevelSpawning : MonoBehaviour {
             return;
         }
 
-        PreviousSpawnLocation = currentLocation;
+        //If doubling back is NOT allowed, calculate a move direction and check if there is a room in that direction
+        //While there isn't an empty room in our requested direction, keep trying directions.
 
-        PreviousMoveDirection = CalculateMoveDirection(level);
-        currentLocation += PreviousMoveDirection;
+        if (!level.CanDoubleBack)
+        {
+            CurrentMoveDirection = CalculateMoveDirection(level);
+
+            while(level.IsRoomAlreadySpawned(currentLocation + CurrentMoveDirection))
+            {
+                //For as long as we are on top of another rooms location, pick a new location and try to spawn there.
+                CurrentMoveDirection = CalculateMoveDirection(level);
+            }
+
+            PreviousSpawnLocation = currentLocation;
+        }
+        if (level.CanDoubleBack)
+        {
+            //If we are allowed to double back, go back to our previous location and try to find an open space
+            currentLocation = PreviousSpawnLocation;
+            CurrentMoveDirection = CalculateMoveDirection(level);
+
+            while(level.IsRoomAlreadySpawned(currentLocation + CurrentMoveDirection))
+            {
+                CurrentMoveDirection = CalculateMoveDirection(level);
+            }
+
+            PreviousSpawnLocation = currentLocation;
+        }
+
+        currentLocation += CurrentMoveDirection;
     }
 
     void SpawnLevelRooms()
@@ -104,49 +143,42 @@ public class LevelSpawning : MonoBehaviour {
         //If there isnt already a room spawned at our location
         if (!TestLevel.IsRoomAlreadySpawned(location))
         {
-            //Debug.Log("No Room Here");
             //Create a new room at our location, name it, and pass it into our room dictionary.
             //Instantiate the room type we generated, and name it accordingly.
             RoomInformation room = new RoomInformation(roomFactory.GrabRoom(CalculateRoomToSpawn(level)), location, "Room" + level.CurrentRoomCount);
-            TestLevel.AddToSpawnedRooms(location, room);
             GameObject go = Instantiate(room.RoomType, location * level.RoomOffset, Quaternion.identity, transform);
+
             if (level.EndRoomSpawned)
             {
                 go.name = "End Room";
                 return;
             }
+
             go.name = room.RoomName;
+            TestLevel.AddToSpawnedRooms(location, room);
             return;
         }
         else
         //If theres a room on top of ours and the level doesn't allow us to backtrack through old rooms.
         {
-            //Debug.Log("Theres a room here");
             if (!level.CanDoubleBack)
             {
-                //Debug.Log("Cant double back");
                 //Generate a room
                 RoomInformation endRoom = new RoomInformation(roomFactory.GrabRoom(CalculateRoomToSpawn(level)), location, "Room" + level.CurrentRoomCount);
 
                 //If we're allowed to spawn an end room
                 if (level.EndRoomSpawned)
                 {
-                    //Debug.Log("Looking to spawn end room");
-                    //And were not currently on top of our starting room
+                    //And were currently on top of our starting room
                     if (currentLocation == startRoomLocation)
                     {
-                        //Debug.Log("On top of spawn room");
-                        //currentLocation += North;
-                        //Delete the room at our current location and replace it with our new room
-                        //level.DeleteRoom(currentLocation);
-                        //level.UpdateRoom(currentLocation, endRoom);
+                        //Move one space north and spawn the final room
                         GameObject go = Instantiate(endRoom.RoomType, (location + North) * level.RoomOffset, Quaternion.identity, transform);
                         go.name = "End Room";
                         return;
                     }
                     else
                     {
-                        //Debug.Log("not on top of spawn room" + currentLocation + " spawn location " + startRoomLocation);
                         //Otherwise, move north and create a new room at that location
                         //currentLocation += North;
                         GameObject go = Instantiate(endRoom.RoomType, (location + North) * level.RoomOffset, Quaternion.identity, transform);
@@ -158,15 +190,19 @@ public class LevelSpawning : MonoBehaviour {
 
             //Otherwise, if we can double back and there is a room in our way
             currentLocation = PreviousSpawnLocation;
-            RoomInformation room = new RoomInformation(roomFactory.GrabRoom(CalculateRoomToSpawn(level)), location, "Room" + level.CurrentRoomCount);
-            GameObject dBgo = Instantiate(room.RoomType, ((location + North) * level.RoomOffset), Quaternion.identity, transform);
+            Vector3 direction = CalculateMoveDirection(level);
+            RoomInformation room = new RoomInformation(roomFactory.GrabRoom(CalculateRoomToSpawn(level)), location + direction, "Room" + level.CurrentRoomCount);
+            level.UpdateRoom(location + direction, room);
+            GameObject dBgo = Instantiate(room.RoomType, ((location + direction) * level.RoomOffset), Quaternion.identity, transform);
+
             if (level.EndRoomSpawned)
             {
                 dBgo.name = "End Room";
+            } else
+            {
+                dBgo.name = room.RoomName;
             }
 
-
-            level.UpdateRoom(location, room);
             return;
         }
     }
@@ -181,7 +217,9 @@ public class LevelSpawning : MonoBehaviour {
         bool eastSpawned = TestLevel.IsRoomAlreadySpawned(currentLocation + East);
         bool westSpawned = TestLevel.IsRoomAlreadySpawned(currentLocation + West);
         bool northSpawned = TestLevel.IsRoomAlreadySpawned(currentLocation + North);
-        bool southSpawned = TestLevel.IsRoomAlreadySpawned(currentLocation + South);
+        
+        //TODO - rework direction picking and spawning
+         
         //Debug.Log(currentLocation + " " + level.MaxZ + " " + level.MaxX);
 
         if (level.GridBasedSpawns) //If we only want to spawn inside our grid...
@@ -288,94 +326,4 @@ public class LevelSpawning : MonoBehaviour {
         //If all else fails, move North
         return North;
     }
-
-    /*
-     * 
-     * else //Otherwise we'll go off of a MaxRoomCount
-        {
-
-            //If we are as far east as we can go and theres a room spawned to the west of us, move north.
-            if (currentLocation.x == level.MaxX && westSpawned && currentLocation.z != level.MaxZ)
-            {
-                return North;
-            }
-            //If we are as far west as we can go and theres a room spawned to the east of us, move north.
-            else if (currentLocation.x == level.MinX && eastSpawned && currentLocation.z != level.MaxZ)
-            {
-                return North;
-            }
-            //If im not at my X or Z bounds, move east or west.
-
-
-            //Need to change this, should check if at Max/Min X - and roll for East/North or West/North accordingly.
-            //Weight will by 70% chance East/West, 30% North.
-            //**Actually just make a bool that will allow for unconstrained spawns and go off of a total room count.
-
-            else if (currentLocation.z != level.MaxZ)
-            {
-                int rand = Random.Range(0, 2);
-
-                if (rand == 0 && !eastSpawned)
-                {
-                    return East;
-                }
-                else if (rand == 1 && !westSpawned)
-                {
-                    return West;
-                }
-            }
-            else if (currentLocation.z == level.MaxZ)
-            {
-                int rand2 = Random.Range(0, 7);
-
-                if (rand2 >= 0 && rand2 <= 2 && !eastSpawned && currentLocation.x != level.MaxX)
-                {
-                    return East;
-                }
-                else if (rand2 >= 3 && rand2 <= 5 && !westSpawned && currentLocation.x != level.MinX)
-                {
-                    return West;
-                }
-                else
-                {
-                    canSpawnEnd = true;
-                }
-            }
-        }
-                //Need to change this, should check if at Max/Min X - and roll for East/North or West/North accordingly.
-        //Weight will by 70% chance East/West, 30% North.
-
-        else if (currentLocation.z != level.MaxZ)
-        {
-            int rand = Random.Range(0, 2);
-
-            if (rand == 0 && !eastSpawned)
-            {
-                return East;
-            }
-            else if (rand == 1 && !westSpawned)
-            {
-                return West;
-            }
-        }
-        else if (currentLocation.z == level.MaxZ)
-        {
-            int rand2 = Random.Range(0, 7);
-
-            if (rand2 >= 0 && rand2 <= 2 && !eastSpawned && currentLocation.x != level.MaxX)
-            {
-                return East;
-            }
-            else if (rand2 >= 3 && rand2 <= 5 && !westSpawned && currentLocation.x != level.MinX)
-            {
-                return West;
-            }
-            else
-            {
-                canSpawnEnd = true;
-            }
-        }
-     * 
-     * 
-     * */
 }
